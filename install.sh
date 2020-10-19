972672fd50a9f052317a30ff72f735772ee0fb15
John R. Spicer <johnrspicer@hotmail.com>
2020-10-19 18:38:13 +1100

    updated i3 and URxvt fontsize

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 # generic applications
-sudo apt install neovim compton xrandr xbacklight ranger caca-utils highlight atool w3m poppler-utils mediainfo xfonts-terminus
+sudo apt install neovim compton xrandr xbacklight ranger caca-utils highlight atool w3m poppler-utils mediainfo xfonts-terminus maim
 
 # special cases for laptop
 ## check for intel_backlight
 sudo find /sys/ -type f -iname '*brightness*'
+
+# install for font-size changing for URxvt
+https://github.com/majutsushi/urxvt-font-size
 
 ## in /etc/X11/xorg.conf add
 Section "Device"
